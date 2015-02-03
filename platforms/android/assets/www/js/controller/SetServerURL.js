24e18ac3c1b6959204f36a49c0607138d1470ce9
hanzhendong <hanzhendong@local.host>
2015-02-03 09:03:38 +0800

    加入HRframe,地址统一处理

---
+++ platforms/android/assets/www/js/controller/SetServerURL.js
@@ -0,0 +1,10 @@
+/**
+ * Created by hanzhendong .
+ */
+$F.regist("SetServerURL", function () {
+    this.SERVER_URL = {
+        //获取最新的地址
+        latestUrl: "http://192.168.1.24:8000/mapi/home/get/latest"
+
+    };
+});
