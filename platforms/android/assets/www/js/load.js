335af14aa1a9b513b615d0b917d660161bc2a195
hanzhendong <hanzhendong@local.host>
2015-02-03 15:04:08 +0800

    页面全部改为load加载，只切换中间区域

---
+++ platforms/android/assets/www/js/load.js
@@ -0,0 +1,31 @@
+/**
+ * Created by hanzhendong on 2015/1/27.
+ */
+//导航切换content内容
+$(document).ready(function () {
+
+    $('#ul li a').click(function () {
+        switch ($(this).text()) {
+            case '新闻':
+                $('#main-content').load('test.html');
+                break;
+            case '推荐':
+                $('#main-content').load('recommend.html');
+                break;
+            case '最热':
+                $('#main-content').load('hottest.html');
+                break;
+            case '最新':
+                $('#main-content').load('latest.html');
+                break;
+            case '消息':
+                $('#main-content').load('message.html');
+                break;
+            default:
+                $('#main-content').load('test.html')
+        }
+
+
+    })
+})
+
