8b5ed57e1f91032c70c47210719a05423c825c66
hanzhendong <hanzhendong@local.host>
2015-02-10 15:01:48 +0800

    活动列表和分享列表加载更多控制

--- platforms/android/assets/www/js/controller/share/getShare.js
+++ platforms/android/assets/www/js/controller/share/getShare.js
@@ -6,6 +6,7 @@ $F.regist("share.getShare", function () {
     var pageNum = parseInt($('#share-content').find('li').length / 10);
     pageNum = pageNum == 0 ? 1 : pageNum + 1;
     console.log(pageNum + "分享页数+++++");
+    $("#fMoreId").hide();
     $.ajax({
         url: $f.get("SERVER_URL").shareUrl,
         type: 'get',
@@ -13,12 +14,16 @@ $F.regist("share.getShare", function () {
         dataType: 'json',
         success: function (data, status) {
             //alert(data);
-            var shareData = data.items
+            var total = data.total;
+            var shareData = data.items;
             console.log("获取分享++++++++++");
             for (var i = 0; i < shareData.length; i++) {
                 var list = "<li><a href='activity_comment.html'><img src='img/tu4.png'/><h2>" + shareData[i].Title + "</h2><P>" + shareData[i].Content + "</P>" + shareData[i].Id + "</a></li>";
                 $("#share-list").append(list);
                 $("#share-list").listview('refresh');
+            }
+            if (total > $('#share-content').find('li').length) {
+                $("fMoreId").show();
             }
         }
     })
